8dac12edc1b543609b0184a46d317388f8f46f18
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-04 20:08:29 -0300

    chore: using optimized next image in logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="logo do Ignite Shop" />
+        <Image src={logoImg} alt="logo do Ignite Shop" />
       </Header>
       <Component {...pageProps} />
     </Container>
